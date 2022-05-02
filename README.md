c2aaba1e606780258b1a765208b8e320015d137f
Kirill Shikhalev <esbraff@yandex.ru>
2022-05-02 18:12:42 +0300

    bewise.ai test

---
+++ README.md
@@ -0,0 +1,13 @@
+# bewise.ai test
+
+---
+
+Start
+
+```shell
+docker-compose up
+```
+
+POST `/create-questions` with `questions_num` specified in request body to create questions
+
+GET `/get-questions` to get all created questions
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass
+import requests
+import config
+
+QUESTIONS_URl = 'https://jservice.io/api/random'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config.POSTGRES_USER}:{config.POSTGRES_PASSWORD}@db/{config.POSTGRES_DB}'
+db = SQLAlchemy(app)
+
+
+@dataclass
+class Question(db.Model):
+    id: int
+    question_text: str
+    answer_text: str
+    created_at: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String)
+    answer_text = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+
+
+db.create_all()
+
+
+@app.route('/create-questions', methods=['POST'])
+def create_questions():
+    try:
+        questions_num = int(request.json['questions_num'])
+        r = requests.get(QUESTIONS_URl, params=dict(count=questions_num))
+        r.raise_for_status()
+
+        for question in r.json():
+            new_question = Question(
+                id=question['id'],
+                question_text=question['question'],
+                answer_text=question['answer'],
+                created_at=question['created_at']
+            )
+            db.session.add(new_question)
+
+        db.session.commit()
+
+        return "OK!"
+    except (ValueError, KeyError):
+        return 'Invalid questions_num value'
+
+
+@app.route('/get-questions', methods=["GET"])
+def get_questions():
+    result = Question.query.all()
+    return jsonify(result)
